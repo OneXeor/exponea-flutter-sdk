9d4c11e0a55654d9172c98f8e5abcb2949d5443b
katerina-demeshko <ekaterina.demeshko@idfinance.com>
2024-09-12 15:45:53 +0200

    version up

--- android/src/main/kotlin/com/exponea/ExponeaPlugin.kt
+++ android/src/main/kotlin/com/exponea/ExponeaPlugin.kt
@@ -785,6 +785,14 @@ class InAppMessageActionStreamHandler private constructor(
         )
     }
 
+    override fun inAppMessageError(message: InAppMessage?, errorMessage: String, context: Context) {
+        // not yet impl
+    }
+
+    override fun inAppMessageShown(message: InAppMessage, context: Context) {
+        // not yet impl
+    }
+
     private fun handle(action: InAppMessageAction): Boolean {
         val sink = eventSink
         if (sink != null) {
